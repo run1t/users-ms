9079fd2586c9b4f3c076e47c0cf954b84daa4fed
Reunan Le Noc <reunan.lenoc@documalis.com>
2018-06-19 18:12:29 +0200

    Send mail from amqp

--- src/controllers/notifications/notifications.controller.ts
+++ src/controllers/notifications/notifications.controller.ts
@@ -1,9 +1,13 @@
 import { NotificationsService } from './../../services/notifications/notifications.service';
 import { Controller, Get, Post, Delete, Put, Param } from '@nestjs/common';
+import { AmqpNotificationsService } from 'services/notifications/amqpNotifications.service';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private readonly notificationsService: NotificationsService) { }
+  constructor(
+    private readonly notificationsService: NotificationsService,
+    private readonly amqpNotificationService: AmqpNotificationsService,
+  ) {}
 
   @Post('send/:userId/:locationId')
   sendMail(@Param('locationId') locationId, @Param('userId') userId) {
