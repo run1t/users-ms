eefc3e6285d6d0f73d0154b84dd37834ad66a46d
Gwenaël HENOT <gwenael.henot@free.fr>
2018-06-19 15:30:08 +0200

    wip: users

--- src/services/users/users.service.ts
+++ src/services/users/users.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UsersService {
-  findAll() {
-    return [
-      0, 1, 2, 3,
-    ];
+  constructor(
+    @InjectModel('User')
+    private readonly userModel: Model<User>,
+  ) { }
+
+  async create(user): Promise<User> {
+    const createdUser = new this.userModel(user);
+    return await createdUser.save();
+  }
+
+  async findAll(): Promise<User[]> {
+    return await this.userModel.find().exec();
+  }
+
+  async delete(id): Promise<any> {
+    return await this.userModel.findByIdAndRemove(id);
+  }
+  async findOne(id): Promise<any> {
+    return await this.userModel.findById(id);
+  }
+  async update(id, update): Promise<any> {
+    return await this.userModel.findByIdAndUpdate(id, update);
   }
 }
